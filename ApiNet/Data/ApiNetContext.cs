10cb9d3cd23d3f159d4afc5d3cfdbb6e1112fb2a
Diegoferreiradev <diegoferreiradev@gmail.com>
2020-07-24 14:40:37 -0300

    Imports removidos

--- ApiNet/Data/ApiNetContext.cs
+++ ApiNet/Data/ApiNetContext.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ApiNet.Models;
 using Microsoft.EntityFrameworkCore;
-using ApiNet.Models;
 
 namespace ApiNet.Data
 {
